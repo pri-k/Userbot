7dd8b9786cea7a91aa2f4c1da42ca9c42cb576df
pri-k <64354404+pri-k@users.noreply.github.com>
2020-06-02 11:53:35 +0530

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -42,7 +42,6 @@ AFKSTR = [
     "I am away!\nI don't know when I'll be back!\nHopefully a few minutes from now!",
     "I'm not available right now so please leave your name, number, and address and I will stalk you later.",
     "Sorry, I'm not here right now.\nFeel free to talk to my userbot as long as you like.\nI'll get back to you later.",
-    "I bet you were expecting an away message!",
     "Life is so short, there are so many things to do...\nI'm away doing one of them..",
     "I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?",
 ]
